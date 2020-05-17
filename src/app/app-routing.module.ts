e31c969849a99ac8f6e07c81d34129e851c8d4a1
Ronald Mansveld <ronald.mansveld@gmail.com>
2020-05-18 00:48:05 +0100

    Rename resonance to constellation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{ path: 'resonance', loadChildren: () => import('./resonance/resonance.module').then(m => m.ResonanceModule) }];
+const routes: Routes = [{ path: 'constellation', loadChildren: () => import('./resonance/resonance.module').then(m => m.ResonanceModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
